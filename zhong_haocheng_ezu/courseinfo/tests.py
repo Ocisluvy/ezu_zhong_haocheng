b5b957b2792e45cfd9e226c822590ca7ee7fb973
Ocisluvy <hzhong11@illinois.edu>
2023-04-26 07:24:52 -0500

    test

---
+++ zhong_haocheng_ezu/courseinfo/tests.py
@@ -0,0 +1,26 @@
+# from django.test import TestCase
+# from django.contrib.auth import get_user_model
+# from .models import Instructor
+# # Create your tests here.
+#
+#
+# class CourseInfoTest(TestCase):
+#     @classmethod
+#     def setUpTestData(cls):
+#         cls.user = get_user_model().objects.create_user(
+#             username="testuser", email="test@email.com", password="secret"
+#         )
+#
+#         cls.instructor1 = Instructor.objects.create(
+#             instructor=1,
+#             first_name="first",
+#             last_name="last",
+#             disambiguator="Harvard"
+#         )
+#
+#         cls.instructor2 = Instructor.objects.create(
+#             instructor=1,
+#             first_name="first",
+#             last_name="last",
+#             disambiguator="Harvard"
+#         )
